afe5db436d4b3c75766843ac35a00e11c97c2385
choijungwoo <cjw7256@gmail.com>
2022-09-19 16:09:36 +0900

    public 스타일시트 부트스트랩추가, 상단 Navbar, Nav.Link 추가

--- src/App.js
+++ src/App.js
@@ -10,11 +10,7 @@ function App() {
         <Container>
           <Navbar.Brand href="/">Market</Navbar.Brand>
           <Nav className="me-auto">
-            {/* <Nav.Link><Link to="/">Home</Link></Nav.Link>
-            <Nav.Link><Link to="/foodDetail">Food Detail</Link></Nav.Link>
-            <Nav.Link><Link to="/info">info</Link></Nav.Link> */}
             <Nav.Link onClick={() => { navigate("/") }}>Home</Nav.Link>
-            {/* <Nav.Link onClick={() => { navigate("/foodDetail") }}>Food Detail</Nav.Link> */}
             <Nav.Link onClick={() => { navigate("/menu") }}>Menu</Nav.Link>
           </Nav>
         </Container>
