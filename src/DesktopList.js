f2fccd56f7fa662c125db2513fa9630bc8344f84
choijungwoo <cjw7256@gmail.com>
2022-09-21 10:22:48 +0900

    axios통신으로  받아오려니까 디테일js에서 정보를 읽어오지 못해서 app.js랑 list.js에 제품정보를 강제로 사용하는방식으로 변경

--- src/DesktopList.js
+++ src/DesktopList.js
@@ -1,54 +1,88 @@
 import './App.css';
-import {
-    Button, Navbar, Nav,
-    Container, Row, Col, Card,
-    DropdownButton, SplitButton,
-    ButtonGroup, Dropdown, Spinner
-} from "react-bootstrap";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Container, Row, } from "react-bootstrap";
+import { Routes, Route, } from "react-router-dom";
 import { useState } from 'react';
 import DesktopCard from './components/DesktopCard.js'
 import ProductDetail from "./components/ProductDetail.js";
 
-import axios from "axios";
 
 function DesktopList() {
-    let navigate = useNavigate();
-    let [getCnt, setGetCnt] = useState(2); // 1 2 3 //get 조회 기준
-    let [showMore, setShowMore] = useState(true); //더보기
-    let [showSpinner, setShowSpinner] = useState(false);
 
 
     let [desktops, setDesktops] = useState([
         {
             id: 'ds001',
-            itemName: 'com001',
+            itemName: '평범해보이지만 비싼거',
             content: '최신형 데스크탑',
             price: '1,000,000원',
             imgPath: "http://192.168.0.63:8898/images/computer1.jpg"
-        },
-        {
+          },
+          {
             id: 'ds002',
-            itemName: 'com002',
+            itemName: '화려해보이지만',
             content: '그저그런 데스크탑',
             price: '800,000원',
             imgPath: "http://192.168.0.63:8898/images/computer2.jpg"
-        },
-        {
+          },
+          {
             id: 'ds003',
-            itemName: 'com003',
+            itemName: '디아블로사양에 맞춘',
             content: '구형 데스크탑',
             price: '600,000원',
             imgPath: "http://192.168.0.63:8898/images/computer3.jpg"
-        },
+          },
+          {
+            id: "ds004",
+            itemName: "굉장히비싼",
+            content: "고성능 데스크탑",
+            price: '1,500,000원',
+            // imgPath: "http://192.168.0.55:8898/images/food4.jpg",
+            imgPath: "http://127.0.0.1:8898/images/computer4.jpg",
+          },
+          {
+            id: "ds005",
+            itemName: "피시방에서 자주본",
+            content: "배틀그라운드 전용 데스크탑",
+            price: "800,000원",
+            // imgPath: "http://192.168.0.55:8898/images/food5.jpg",
+            imgPath: "http://127.0.0.1:8898/images/computer5.jpg",
+          },
+          {
+            id: "ds006",
+            itemName: "그래픽카드만 좋은",
+            content: "이상한 데스크탑",
+            price: "700,000원",
+            // imgPath: "http://192.168.0.55:8898/images/food6.jpg",
+            imgPath: "http://127.0.0.1:8898/images/computer6.jpg",
+          },
+          {
+            id: "ds007",
+            itemName: "아파트처럼 생긴",
+            content: "별거없는 데스크탑",
+            price: "600,000원",
+            // imgPath: "http://192.168.0.55:8898/images/food7.jpg",
+            imgPath: "http://127.0.0.1:8898/images/computer7.jpg",
+          },
+          {
+            id: "ds008",
+            itemName: "흰색이라 때가잘타는",
+            content: "비싼 데스크탑",
+            price: "1,200,000원",
+            // imgPath: "http://192.168.0.55:8898/images/food8.jpg",
+            imgPath: "http://127.0.0.1:8898/images/computer8.jpg",
+          },
+          {
+            id: "ds009",
+            itemName: "블링블링",
+            content: "이쁜 분홍색 데스크탑",
+            price: "1,300,000원",
+            // imgPath: "http://192.168.0.55:8898/images/food9.jpg",
+            imgPath: "http://127.0.0.1:8898/images/computer9.jpg",
+          },
 
     ])
 
-
-
-
-
-
+    
     return (
         <div className="App">
             <Routes>
@@ -66,67 +100,7 @@ function DesktopList() {
                                     })}
                                 </Row>
                             </Container>
-
-                            {
-                                //더보기를 눌렀을때~ 로딩중일때~
-                                //눌렀을때 스피너가 돌고
-                                //끝나면 스피너가 사라지고
-                                showSpinner == true ? (
-                                    <div>
-                                        <Spinner animation="border" role="status">
-                                            <span className="visually-hidden">Loading...</span>
-                                        </Spinner>
-                                    </div>
-                                ) : null
-                            }
-
-                            {showMore == true ? (
-                                <Button
-                                    variant="success"
-                                    onClick={() => {
-                                        const urlPath =
-                                            "http://192.168.0.63:8898/desktopData/list" +
-                                            getCnt + //2
-                                            ".json"
-                                        //2 호출 -> 3 호출
-                                        //3
-                                        setShowSpinner(true);
-                                        axios
-                                            .get(urlPath)
-                                            .then((value) => {
-                                                // console.log(value.data[0]);  //출력하려는 데이터 조회 from server
-                                                let temp = [...desktops]; // [1,2,3,4]
-                                                for (
-                                                    let i = 0;
-                                                    i < value.data.length;
-                                                    i++ //3, 0 1 2
-                                                ) {
-                                                    temp.push(value.data[i]); //배열에 하나씩 넣어야하는 객체
-                                                }
-                                                setDesktops(temp);
-                                                //getCnt = 3
-                                                const tempCnt = getCnt + 1;
-                                                setGetCnt(getCnt + 1);
-                                                if (tempCnt > 3) {
-                                                    setShowMore(false);
-                                                }
-                                                // setShowSpinner(false);
-                                            })
-                                            .catch(() => {
-                                                console.log("뭔가 잘못된거");
-                                                // setShowSpinner(false);
-                                            })
-                                            .finally(() => {
-                                                setShowSpinner(false);
-                                            })
-                                        //서버 통신 axios.get 가져와서
-                                        //foods 배열 추가
-                                        // setShowMore(false);
-                                    }}
-                                >
-                                    더보기...
-                                </Button>
-                            ) : null}
+                           
                         </div>
                     }
                 />
