a539057ca31d4b685ba408a6243664d754babb7f
choijungwoo <cjw7256@gmail.com>
2022-09-27 15:06:18 +0900

    데스크탑 자료수정

--- src/DesktopList.js
+++ src/DesktopList.js
@@ -7,90 +7,81 @@ import ProductDetail from "./components/ProductDetail.js";
 
 
 function DesktopList() {
-
-
   let [desktops, setDesktop] = useState([
 
     {
       id: 'ds001',
-      itemName: '평범해보이지만 비싼거',
-      content: '최신형 데스크탑',
-      price: '1,000,000원',
-      price2: 1000000,
+      itemName: 'DELL 3080 SFF WiFi i5-10505 8G 256G 윈10 Pro',
+      content: 'CPU:인텔 코어 i5-10505\n저장장치:SSD 256GB\n메모리:8GB DDR4\n그래픽:intel UHD Graphics',
+      price: '799,000원',
+      price2: 799000,
       imgPath: "http://192.168.0.63:8898/images/computer1.jpg"
     },
     {
       id: 'ds002',
-      itemName: '화려해보이지만',
-      content: '그저그런 데스크탑',
-      price: '800,000원',
-      price2: 800000,
+      itemName: '컴집/게이밍/조립PC',
+      content: 'CPU:AMD 라이젠 5-4세대 5600X 3.7G\n저장장치:SSD 512GB\n메모리:16GB DDR4\n그래픽:지포스 RTX3060 D6 12G',
+      price: '1,029,000원',
+      price2: 1029000,
       imgPath: "http://192.168.0.63:8898/images/computer2.jpg"
     },
     {
       id: 'ds003',
-      itemName: '디아블로사양에 맞춘',
-      content: '구형 데스크탑',
-      price: '600,000원',
-      price2: 600000,
+      itemName: '컴집/게이밍/조립PC',
+      content: 'CPU:인텔12세대-i5 12400F 2.5G\n저장장치:SSD 512GB\n메모리:16GB DDR4\n그래픽:지포스 RTX3060 D6 12G',
+      price: '1,069,000원',
+      price2: 1069000,
       imgPath: "http://192.168.0.63:8898/images/computer3.jpg"
+    },    
+    {
+      id: 'ds004',
+      itemName: 'NEW.신상 특별한이벤트.삼성정품.DM500SD/사은품증정~최다판매.고성능NVMe',
+      content: 'CPU:인텔 12세대 Pentium 듀얼코어 G7400\n저장장치:SSD 256GB\n메모리:4GB DDR4\n그래픽:intel UHD Graphics 710',
+      price: '648,000원',
+      price2: 648000,
+      imgPath: "http://192.168.0.63:8898/images/computer4.jpg"
     },
     {
-      id: "ds004",
-      itemName: "굉장히비싼",
-      content: "고성능 데스크탑",
-      price: '1,500,000원',
-      price2: 1500000,
-      // imgPath: "http://192.168.0.55:8898/images/food4.jpg",
-      imgPath: "http://127.0.0.1:8898/images/computer4.jpg",
+      id: 'ds005',
+      itemName: 'LG일체형PC 27V70N-GR3SK 27인치 10세대i3 8GB 256GB win11 올인원 PC 데스크탑 컴퓨터',
+      content: 'CPU:10세대 인텔 코어 i3-10110U\n저장장치:SSD 256GB\n메모리:8GB DDR4\n그래픽:intel UHD Graphics',
+      price: '929,000원',
+      price2: 929000,
+      imgPath: "http://192.168.0.63:8898/images/computer5.jpg"
     },
     {
-      id: "ds005",
-      itemName: "피시방에서 자주본",
-      content: "배틀그라운드 전용 데스크탑",
-      price: "800,000원",
-      price2: 800000,
-      // imgPath: "http://192.168.0.55:8898/images/food5.jpg",
-      imgPath: "http://127.0.0.1:8898/images/computer5.jpg",
+      id: 'ds006',
+      itemName: 'DELL 3080 SFF WiFi i5-10505 8G 256G 윈10 Pro',
+      content: 'CPU:인텔 코어 i3-1115G4 Processor\n저장장치:SSD 256GB\n메모리:8GB DDR4\n그래픽:intel UHD Graphics',
+      price: '858,000원',
+      price2: 858000,
+      imgPath: "http://192.168.0.63:8898/images/computer6.jpg"
     },
     {
-      id: "ds006",
-      itemName: "그래픽카드만 좋은",
-      content: "이상한 데스크탑",
-      price: "700,000원",
-      price2: 700000,
-      // imgPath: "http://192.168.0.55:8898/images/food6.jpg",
-      imgPath: "http://127.0.0.1:8898/images/computer6.jpg",
+      id: 'ds007',
+      itemName: '굿워크 D214N 인텔 10th i5-10400 데스크탑 그레이',
+      content: 'CPU:10세대 인텔 코어 i5-10400\n저장장치:SSD 240GB\n메모리:8GB DDR4\n그래픽:intel UHD 630',
+      price: '630,520원',
+      price2: 630520,
+      imgPath: "http://192.168.0.63:8898/images/computer7.jpg"
     },
     {
-      id: "ds007",
-      itemName: "아파트처럼 생긴",
-      content: "별거없는 데스크탑",
-      price: "600,000원",
-      price2: 600000,
-      // imgPath: "http://192.168.0.55:8898/images/food7.jpg",
-      imgPath: "http://127.0.0.1:8898/images/computer7.jpg",
+      id: 'ds008',
+      itemName: '리오나인터보 CG256XR66 라이젠5600X RX6600 게이밍PC',
+      content: 'CPU:AMD Ryzen5 5600X 버미어\n저장장치:SSD 512GB\n메모리:16GB DDR4\n그래픽:AMD 라데온 RX 6600',
+      price: '1,156,840원',
+      price2: 1156840,
+      imgPath: "http://192.168.0.63:8898/images/computer8.jpg"
     },
     {
-      id: "ds008",
-      itemName: "흰색이라 때가잘타는",
-      content: "비싼 데스크탑",
-      price: "1,200,000원",
-      price2: 1200000,
-      // imgPath: "http://192.168.0.55:8898/images/food8.jpg",
-      imgPath: "http://127.0.0.1:8898/images/computer8.jpg",
-    },
-    {
-      id: "ds009",
-      itemName: "블링블링",
-      content: "이쁜 분홍색 데스크탑",
-      price: "1,300,000원",
-      price2: 1300000,
-      // imgPath: "http://192.168.0.55:8898/images/food9.jpg",
-      imgPath: "http://127.0.0.1:8898/images/computer9.jpg",
-    },
+      id: 'ds009',
+      itemName: '라이젠 R5 5600X_GTX1660SUPER 게이밍컴퓨터 조립PC',
+      content: 'CPU:라이젠 R5 5600X\n저장장치:SSD 512GB\n메모리:16GB DDR4\n그래픽:지포스 GTX 1660 SUPER 6GB',
+      price: '889,000원',
+      price2: 889000,
+      imgPath: "http://192.168.0.63:8898/images/computer9.jpg"
+    },    
   ])
-
 
   return (
     <div className="App">
